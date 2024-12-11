3a0cbb80a0a2ba0750ce6ee4c6778a59c58100d4
AALOK PATEL <aalok.patel@primedemo.in>
2024-12-11 18:00:16 +0530

    added postinstall scripts

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,9 @@ const nextConfig: NextConfig = {
     typescript: {
         ignoreBuildErrors: true,
     },
+    experimental: {
+        serverComponentsExternalPackages: ["@prisma/client"],
+    },
 };
 
 export default nextConfig;
